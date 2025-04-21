abe24ca9ec63ef6fb17049e8d019a3e6442b552a
CharukaVithna <clvithana2001@gmail.com>
2025-04-22 01:41:36 +0530

    Main Setup and added CreateRecipe , GetRecipe functionality

---
+++ CookbookApp.APi/Data/CookbookDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CookbookApp.APi.Models;
+
+namespace CookbookApp.APi.Data
+{
+    public class CookbookDbContext : DbContext
+    {
+        public CookbookDbContext(DbContextOptions dbContextOptions): base(dbContextOptions) { }
+
+        public DbSet<Recipe> Recipes { get; set; }
+
+    }
+}
