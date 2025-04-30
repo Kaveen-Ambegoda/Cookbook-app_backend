6c33784589be2868984744568e463d95463c9952
CharukaVithna <clvithana2001@gmail.com>
2025-04-30 16:14:44 +0530

    Recipe Controller Functions changed

--- CookbookApp.APi/Controllers/RecipeController.cs
+++ CookbookApp.APi/Controllers/RecipeController.cs
@@ -20,6 +20,7 @@ namespace CookbookApp.APi.Controllers
             this._context = dbContext;
         }
 
+        //Save Recipe
         
         [HttpPost("AddRecipe")]
         public async Task<IActionResult> AddRecipe([FromBody] Recipe recipe)
@@ -43,7 +44,7 @@ namespace CookbookApp.APi.Controllers
 
         //Get all recipes
 
-        [HttpGet]
+        [HttpGet("recipeMangePage")]
         public IActionResult GetAllRecipes()
         {
             // Get data from the database
@@ -186,109 +187,9 @@ namespace CookbookApp.APi.Controllers
             }
         }
 
+        //Get All Recipies to HomePage
 
-        /*
         
-        //GET all recipes
-        [HttpGet]
-        public IActionResult GetAllRecipes()
-        {
-            // Get data from the database
-            var recipesDomain = _context.Recipes.ToList();
-
-            // Map domain models to DTO
-            var recipesDto = new List<RecipeDto>();
-
-            foreach (var recipeDomain in recipesDomain)
-            {
-                recipesDto.Add(new RecipeDto()
-                {
-                    Id = recipeDomain.Id,
-                    Title = recipeDomain.Title,
-                    ImageUrl = recipeDomain.ImageUrl,
-                    CookingTime = recipeDomain.CookingTime,
-                    Portion = recipeDomain.Portion,
-                });
-            }
-
-            return Ok(recipesDto);
-        }
-        */
-
-
-        /*
-
-        //GET single recipe by ID
-        [HttpGet("{id}")]
-        public IActionResult GetRecipeById(int id)
-        {
-            var recipeDomain = _context.Recipes.FirstOrDefault(x => x.Id==id);
-            if (recipeDomain == null)
-                return NotFound();
-
-            //Map Recipe Domain to DTO
-
-            var recipeDto = new RecipeDto
-            {
-                Id = recipeDomain.Id,
-                Name = recipeDomain.Name,
-                ImageUrl = recipeDomain.ImageUrl,
-                CookingTime = recipeDomain.CookingTime,
-                Servings = recipeDomain.Servings,
-            };
-            //return DTO back 
-            return Ok(recipeDto);
-        }
-        [HttpPost]
-        public IActionResult Create([FromBody] AddRecipeRequestDto addRecipeRequestDto)
-        {
-            //Map Dto to Domain
-            var recipeDomainModel = new Recipe
-            {
-                Name = addRecipeRequestDto.Name,
-                ImageUrl = addRecipeRequestDto.ImageUrl,
-                CookingTime = addRecipeRequestDto.CookingTime,
-                Servings = addRecipeRequestDto.Servings,
-            };
-
-            //Use Domain Model to create Recipe
-            _context.Recipes.Add(recipeDomainModel);
-            _context.SaveChanges();
-            //Map Domain model back to Dto
-            var recipeDto = new RecipeDto
-            {
-                Id = recipeDomainModel.Id,
-                Name = recipeDomainModel.Name,
-                ImageUrl = recipeDomainModel.ImageUrl,
-                CookingTime = recipeDomainModel.CookingTime,
-                Servings = recipeDomainModel.Servings,
-            };
-
-            return CreatedAtAction(nameof(GetRecipeById), new {id=recipeDomainModel.Id}, recipeDomainModel);
-        }
-        */
-
-        [HttpGet("searchRecipe/{id}")]
-        public async Task<IActionResult> SearchRecipe(int id)
-        {
-            try
-            {
-                var recipe = await _context.Recipes.FindAsync(id);
-
-                if (recipe == null)
-                    return NotFound(); // Handle if no recipe is found
-
-                return Ok(recipe); // Return recipe if found
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new
-                {
-                    Error = ex.Message
-                });
-            }
-        }
-
 
     }
 }
--- CookbookApp.APi/Models/DTO/GetHomeRecipeDto.cs
+++ CookbookApp.APi/Models/DTO/GetHomeRecipeDto.cs
@@ -1,8 +1,8 @@
 ﻿namespace CookbookApp.APi.Models.DTO
 {
-    public class GetRecipeDto
+    public class GetHomeRecipeDto
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
         public string Title { get; set; }
         public int CookingTime { get; set; }
         public int Portion { get; set; }
